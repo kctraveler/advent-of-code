dd4006d4de88aadcea211778936f54d0920902fc
Shane Panchot <shane.panchot@gmail.com>
2022-12-16 13:09:29 -0600

    day4 part 2 complete

--- day4/main.py
+++ day4/main.py
@@ -4,9 +4,13 @@ with open('./advent-of-code/day4/input.txt') as f:
         elfRanges = line.rstrip().split(",")
         elfOne, elfTwo = tuple(map(int, elfRanges[0].split("-"))), tuple(map(int,elfRanges[1].split("-")))
         # check if elfOne contained by elfTwo
-        if (elfOne[0] >= elfTwo[0] and elfOne[0] <= elfTwo[-1]) and ( elfOne[-1] <= elfTwo[-1]):
-            result +=1
+        #if (elfOne[0] >= elfTwo[0] and elfOne[0] <= elfTwo[-1]) and ( elfOne[-1] <= elfTwo[-1]):
+        #    result +=1
         # Check if elfTwo contained by elfOne
-        elif (elfTwo[0] >= elfOne[0] and elfTwo[0] <= elfOne[-1]) and (elfTwo[-1] <= elfOne[-1]):
-            result +=1 
+        #elif (elfTwo[0] >= elfOne[0] and elfTwo[0] <= elfOne[-1]) and (elfTwo[-1] <= elfOne[-1]):
+        #   result +=1 
+        
+        #check for any overlap
+        if (elfOne[0] >= elfTwo[0] and elfOne[0] <= elfTwo[-1]) or (elfTwo[0] >= elfOne[0] and elfTwo[0] <= elfOne[-1]):
+            result += 1
 print("Number of groups that completely overlap: %d" % result)
