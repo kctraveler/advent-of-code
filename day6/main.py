d0317d373323ba4765547ca3d5ea45b22bf406f8
Shane Panchot <shane.panchot@gmail.com>
2022-12-19 23:14:02 -0600

    day6 part 1 complete

---
+++ day6/main.py
@@ -0,0 +1,23 @@
+def identifyFrame(input, frameSize=4):
+    input = list(input)
+    frame = list(input[0])
+    i = 1
+    while i < len(input):
+        if input[i] in frame: #duplicate found, shift the frame
+            for j in range(frame.index(input[i])+1):
+                frame.remove(frame[0])
+            frame.append(input[i])
+            i+=1
+        elif len(frame) < 4: # frame too short, add the element
+            frame.append(input[i])
+            i+=1
+        else: #frame found return the index
+            return i
+        print(frame)
+
+def main():
+    with open("./advent-of-code/day6/input.txt") as f:
+        input = f.read()
+    print(identifyFrame(input))
+    
+main()
